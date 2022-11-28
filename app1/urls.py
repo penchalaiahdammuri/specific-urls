8438a146b6590f2fd7f707653b711f8202ce58ae
penchalaiahdammuri <penchalaiahdammuri@gmail.com>
2022-11-28 20:16:27 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from app1.views import *
+app_name='app1'
+urlpatterns=[
+    path('a1_first/',a1_first,name='a1_first'),
+    path('a1_second/',a1_second,name='a1_second'),
+]
